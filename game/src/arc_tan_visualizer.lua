f44caa96f798dd64ca0ce3d40293aa1549bb3595
ZS22007412 <ZS22007412@gmail.com>
2026-01-31 14:14:01 -0600

    Arc Tan Visualizer: - Refactored entire project - Added new todo list

--- game/src/arc_tan_visualizer.lua
+++ game/src/arc_tan_visualizer.lua
@@ -4,246 +4,111 @@
 ]]
 
 --[[ Todo List:
-	- [x] Bundle both circle drawing functions on one
-	- [ ] Make the dimensions dynamic according to the screen size
-	- [ ] Refactor function with local variables
-	- [ ] Fix the memory leak
+	- [ ] Add the cos function
+  - [ ] Refactor the figures to have a better structure
+	- [ ] Add lines connecting the relay circles and figures
+	- [ ] Fix the graphical bug on the relay figures
+    - Try and add the coords every 5 frames instead on each one
 ]]
 
 local arc = {}
 
+local window = {}
+window.width, window.height = love.window.getDesktopDimensions()
+local game = {}
+game.width, game.height = math.floor(window.width * 0.66), math.floor(window.height * 0.66)
+
+local mainCircle = {
+  center_x = game.width/6,
+  center_y = game.height/2,
+  radius   = game.height/6,
+}
+
+function mainCircle:draw()
+  love.graphics.push()
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setLineWidth(game.height/32)
+  love.graphics.circle("line", self.center_x, self.center_y, self.radius)
+  love.graphics.pop()
+end
+
+local relayCircle = {
+  list = {}
+}
+
+function relayCircle:create(x, y, color)
+  local circle = {
+    x = x,
+    y = y,
+    size = game.height/32,
+    color = color or {1, 1, 1},
+    angle = 0,
+    speed = 100,
+    figure = {} -- List of figure points
+  }
+
+  table.insert(self.list, circle)
+end
+
+function relayCircle:draw()
+  for i, c in ipairs(self.list) do
+    love.graphics.push()
+    love.graphics.setColor(c.color)
+    love.graphics.circle('fill', math.floor(c.x), math.floor(c.y), c.size)
+    love.graphics.pop()
+
+    love.graphics.print("angle: " .. math.floor(c.angle) .. "°", game.height/8, i*game.height/16) 
+
+    if #c.figure > 2 then
+      love.graphics.line(c.figure)
+    end
+  end
+end
+
+function relayCircle:update(dt)
+  for i, c in ipairs(self.list) do
+    -- Update cardinal coords based on polar coords
+    c.x = mainCircle.center_x + mainCircle.radius*math.cos(math.rad(c.angle))
+    c.y = mainCircle.center_y - mainCircle.radius*math.sin(math.rad(c.angle))
+    
+    -- Insert the new position on the figure table
+    table.insert(c.figure, game.height/2) 
+    table.insert(c.figure, c.y)
+    
+
+    for j, f in ipairs(c.figure) do
+      if j%2== 1 then
+        c.figure[j] = c.figure[j] + 100 * dt
+
+        if c.figure[j] > game.width/2 then -- Remove out of screen figures
+          table.remove(c.figure, j)
+          table.remove(c.figure, j)
+        end
+      end
+    end
+
+    -- Update the angle
+    if c.angle > 360 then c.angle = 0.1
+    else c.angle = c.angle + c.speed * dt end
+  end
+end
+
 function arc.load()
-  font = love.graphics.newFont("assets/arial.ttf", 40)
-  success = love.window.setMode( "1280", "512" )
-  
-  -- Main Circle
-  list_circles = {}
-  min_x, min_y = 50, 50
-  max_x, max_y = 500, 500
-  num_circles = 360
-  spacing = 70  
-  radius = 150
-  
-  center_x, center_y = (max_x - min_x) / 2 + min_x, (max_y - min_y) / 2 + min_y
-  
-  createCircleOfCircles()
-  
-  -- Moving Circles
-  list_sm_circles = {}
-  list_cm_circles = {}
-  m_circle_h = 600
-  
-  --
-  sin_index = num_circles / 2 + 1
-  cosin_index = num_circles / 4 * 3 + 1
-  s_countdown = 3
-  c_countdown = 3
-  
-  -- Relay Circles
-  list_sr_circles = {}
-  list_sr_lines = {}
-  
-  list_cr_circles = {}
-  list_cr_lines = {}
-  
-  max_countdown = 1
-  
+  love.graphics.setFont(love.graphics.newFont("assets/arial.ttf", 40))
+  love.window.setMode(game.width, game.height)
+
+  -- Sin relay circle
+  relayCircle:create(mainCircle.center_x+mainCircle.radius, mainCircle.center_y, {0, 0, 1})
+  -- Todo: Cos relay circle
 end
 
 function arc.update(dt)
-  for i, circle in ipairs(list_sm_circles) do
-    circle.x = circle.x + circle.speed * dt
-  end
-  for i, circle in ipairs(list_cm_circles) do
-    circle.x = circle.x + circle.speed * dt
-  end
-  
-  if sin_index < table.maxn(list_circles) + 1 then
-    --createSinMovingCircle(list_circles[sin_index].real_x, list_circles[sin_index].real_y)
-    createSinMovingCircle(m_circle_h, list_circles[sin_index].real_y)
-    createSinRelayCircle(list_circles[sin_index].real_x, list_circles[sin_index].real_y)
-  end
-  
-  if cosin_index < table.maxn(list_circles) + 1 then
-    --createSinMovingCircle(list_circles[sin_index].real_x, list_circles[sin_index].real_y)
-    createCosinMovingCircle(m_circle_h, list_circles[cosin_index].real_y)
-    createCosinRelayCircle(list_circles[cosin_index].real_x, list_circles[cosin_index].real_y)
-  end
-  
-  if s_countdown == 0 then
-    if sin_index > table.maxn(list_circles) - 1 then
-      sin_index = 1
-    else
-      sin_index = sin_index + 1
-    end
-    s_countdown = max_countdown
-  else
-    s_countdown = s_countdown - 1
-  end
-  
-  if c_countdown == 0 then
-    if cosin_index > table.maxn(list_circles) - 1 then
-      cosin_index = 1
-    else
-      cosin_index = cosin_index + 1
-    end
-    c_countdown = max_countdown
-  else
-    c_countdown = c_countdown - 1
-  end
-  
+  relayCircle:update(dt)
 end
 
 function arc.draw()
-	local drawCircle = function (size, x, y)
-		local px
-		if size == 'small' then px = 10 end
-		if size == 'big'   then px = 20 end
-		love.graphics.circle('fill', x, y, px)
-	end	
-
-  love.graphics.setFont(font)
-  -- Debug Text
-  love.graphics.setColor(1, 1, 1)
-  --love.graphics.print("x: " .. tostring(love.mouse.getX()), 10, 10)
-  --love.graphics.print("y: " .. tostring(love.mouse.getY()), 10, 25)
-  if sin_index > 180 then
-    angle = sin_index - 180
-  else
-    angle = sin_index + 180
-  end
-  love.graphics.print("angle: " .. angle .. "°", 30, 30)
-  
-  -- Center Circle
-  love.graphics.setColor(1, 0, 0)
-  drawCircle('small', center_x, center_y)
-  
-  -- Main Circle
-  love.graphics.setColor(1, 1, 1)
-  for i, circle in ipairs(list_circles) do 
-    drawCircle('small', circle.real_x, circle.real_y)
-  end
-  
-  
-  for i, circle in ipairs(list_cr_circles) do 
-    love.graphics.setColor(1, 0.5, 0.5)
-    drawRelayLines(circle.x, circle.y)
-    love.graphics.setColor(1, 0, 0)
-    drawCircle('big', circle.x, circle.y)
-    
-    table.remove(list_cr_circles, 1)
-    
-  end
-  
-  for i, circle in ipairs(list_sr_circles) do 
-    love.graphics.setColor(0.5, 0.5, 1)
-    drawRelayLines(circle.x, circle.y)
-    
-    love.graphics.setColor(0, 0, 1)
-    drawCircle('big', circle.x, circle.y)
-    
-    table.remove(list_sr_circles, 1)
-    
-  end
-  
-  -- Moving Circles
-  love.graphics.setColor(1, 0, 0)
-  for i, circle in ipairs(list_cm_circles) do 
-    drawCircle('small', circle.x, circle.y)
-  end
-  
-  love.graphics.setColor(0, 0, 1)
-  for i, circle in ipairs(list_sm_circles) do 
-    drawCircle('small', circle.x, circle.y)
-  end
-end
-
-function createCircle(x, y)
-  circle = {}
-  circle.x = x
-  circle.y = y
-  
-  circle.distance = math.sqrt((center_x - x) ^ 2 + (center_y - y) ^ 2)
-  
-  vx, vy = center_x - circle.x, center_y - circle.y
-  
-  if (vx > 0 and vy > 0) or (vx < 0 and vy < 0) then
-    total_vector = math.abs(vx + vy)
-  else
-    total_vector = math.abs(vx - vy)
-  end
-  
-  circle.vx_norm, circle.vy_norm = vx /  circle.distance, vy / circle.distance
-  
-  vx_norm, vy_norm = circle.vx_norm, circle.vy_norm
-  
-  circle.real_x = center_x + radius * circle.vx_norm
-  circle.real_y = center_y + radius * circle.vy_norm
-  
-  table.insert(list_circles, circle)
-  
-end
-
-function createCircleOfCircles()
-  
-  --[[
-  for x=min_x, max_x, spacing do createCircle(x, min_y) end
-  for y=min_y, max_y, spacing do createCircle(max_x, y) end
-  
-  for x=max_x, min_x, -spacing do createCircle(x, max_y) end
-  for y=max_y, min_y, -spacing do createCircle(min_x, y) end
-  ]]
-  
-  for i = num_circles - 1, 0, -1 do
-    local angle = (2 * math.pi / num_circles) * i -- Calculate angle in radians
-    local x = center_x + radius * math.cos(angle) -- X position
-    local y = center_y + radius * math.sin(angle) -- Y position
-    createCircle(x, y)
-  end
-end
-
-
-
-function drawRelayLines(x, y)
-  love.graphics.rectangle("fill", x, y- 5, m_circle_h - x, 10)
-end
-
-function createSinMovingCircle(x, y)
-  m_circle = {}
-  m_circle.x = x
-  m_circle.y = y
-  
-  m_circle.speed = 100
-  m_circle.lifespan = 5000
-  
-  table.insert(list_sm_circles, m_circle)
-end
-
-function createSinRelayCircle(x, y)
-  r_circle = {}
-  r_circle.x = x
-  r_circle.y = y
-  
-  table.insert(list_sr_circles, r_circle)
-end
-
-function createCosinMovingCircle(x, y)
-  m_circle = {}
-  m_circle.x = x
-  m_circle.y = y
-  
-  m_circle.speed = 100
-  m_circle.lifespan = 5000
-  
-  table.insert(list_cm_circles, m_circle)
-end
-
-function createCosinRelayCircle(x, y)
-  r_circle = {}
-  r_circle.x = x
-  r_circle.y = y
-  
-  table.insert(list_cr_circles, r_circle)
+  mainCircle:draw()
+  relayCircle:draw()
 end
 
 return arc
